a995e1c21e7c68d6f084a09b6152eff122ed6c65
Jason40004 <148589763+JasonCourtois@users.noreply.github.com>
2024-07-01 16:54:43 -0400

    okay epic

--- static/myapp/js/upload_file.js
+++ static/myapp/js/upload_file.js
@@ -1,12 +1,16 @@
 // Get needed refrences to document objects
-const settingsButton = document.querySelector(".settings-button");  // Gear icon in top right
-const settingsWrapper = document.querySelector(".settings-container");  // Wrapper for settings area
-const graphContainer = document.getElementById("graph-container");  // Container for Graph and Settings area
-const uploadWrapper = document.getElementById("upload-container");  // Container for the upload file button
-const selectedPoints = document.getElementById("selected-section"); // Wrapper for selected points below graph
-const graph = document.getElementById("graph");
-const importNavButton = document.getElementById("nav-import");
+const settingsButton = document.querySelector(".settings-button"); // Gear icon in top right
+const settingsContainer = document.querySelector(".settings-container"); // Container for settings area
+const graphAndSettingsContainer = document.getElementById(
+  "graph-settings-container"
+); // Container for Graph and Settings area
+const uploadContainer = document.getElementById("upload-container"); // Container for the upload file button
+const selectedPoints = document.getElementById("selected-section"); // Container for selected points below graph
+const graph = document.getElementById("graph"); // Graph iframe
+const importNavButton = document.getElementById("nav-import"); // nav button in top right
+const fileInput = document.getElementById("initial-file-input"); // Select file button
 
+// This becomes a refrence to the iframe once it has loaded
 let frame;
 
 graph.onload = () => {
@@ -14,87 +18,102 @@ graph.onload = () => {
   main();
 };
 
+// Entry point
 async function main() {
+  // If there is alreadt a file and UMAP data in local storage, call iframe.main()
   if (
     localStorage.getItem("data") !== null &&
     localStorage.getItem("rawFile") !== null
   ) {
     await frame.main();
- 
+
+    // Hide upload and show graph
     hideUpload();
     showGraph();
   } else {
+    // Show upload screen
     showUpload();
   }
 }
 
+// Import file button script
 function importFile() {
+  // Warn user that if they have data stored, it will be deleted
   if (
     localStorage.getItem("data") === null ||
     confirm("Do you want to import new data?\nThis erases current saved data.")
   ) {
+    // Clear selected points and remove local storage
     frame.clearSelected();
     localStorage.removeItem("rawFile");
     localStorage.removeItem("camera");
     localStorage.removeItem("data");
     localStorage.removeItem("annotations");
+    // Show upload screen
     hideGraph();
     showUpload();
   }
 }
 
+// Redirect to help page for help button
 function redirectToHelp() {
   window.location.href = "/help";
 }
 
+// When the user clicks the settings button, add classes to trigger css animation
 settingsButton.addEventListener("click", function () {
-  settingsWrapper.classList.toggle("visible");
+  settingsContainer.classList.toggle("visible");
   graph.classList.toggle("settings-open");
 });
 
-document
-  .getElementById("initial-file-input")
-  .addEventListener("change", function (event) {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-    reader.onload = function (e) {
-      const fileData = e.target.result;
-      localStorage.setItem("rawFile", fileData);
-    };
-    reader.readAsDataURL(file);
-  });
+// When user selects a file, save it in local storage in base64 to send to backend endpoint
+fileInput.addEventListener("change", function (event) {
+  const file = event.target.files[0];
+  const reader = new FileReader();
+  reader.onload = function (e) {
+    const fileData = e.target.result;
+    localStorage.setItem("rawFile", fileData);
+  };
+  reader.readAsDataURL(file);
+});
 
+// When user clicks upload, call iframe.main() which will use file in local storage
 document
   .getElementById("initial-upload-button")
   .addEventListener("click", async function () {
-    if (!localStorage.getItem("rawFile")) {
+    // If there is no file saved or selected, return
+    if (!localStorage.getItem("rawFile") || fileInput.value === "") {
       return;
     }
+    
+    // Try to draw graph, catch any error that comes up and display it
     try {
       await frame.main();
     } catch (error) {
       alert(`Check that file was formatted correctly, error: ${error}`);
       return;
     }
-    document.getElementById("initial-file-input").value = "";
+
+    // Clear the fileInput button and show graph
+    fileInput.value = "";
     hideUpload();
     showGraph();
   });
 
 function showGraph() {
-  graphContainer.style.display = "flex";
+  graphAndSettingsContainer.style.display = "flex";
   selectedPoints.style.display = "block";
 }
 
 function hideGraph() {
-  graphContainer.style.display = "none";
+  graphAndSettingsContainer.style.display = "none";
   selectedPoints.style.display = "none";
 }
 
 function showUpload() {
-  uploadWrapper.style.display = "flex";
+  uploadContainer.style.display = "flex";
 }
 
 function hideUpload() {
-  uploadWrapper.style.display = "none";
+  uploadContainer.style.display = "none";
 }
